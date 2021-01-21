41bb72fceaa6146082241c93d7c66235d364cd5f
Spencer Goad <spencer@noviams.com>
2021-01-21 16:13:19 -0500

    Add global settings class for web.config entries.

--- NoviInterviewMiniProject/NoviInterviewMiniProject/Controllers/HomeController.cs
+++ NoviInterviewMiniProject/NoviInterviewMiniProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NoviInterviewMiniProject.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,16 @@ namespace NoviInterviewMiniProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IGlobalSettings _globalSettings;
+
+        public HomeController(IGlobalSettings globalSettings)
+        {
+            _globalSettings = globalSettings;
+        }
         public ActionResult Index()
         {
+            ViewBag.ApiUrl = _globalSettings.ApiUrl;
+
             return View();
         }
 
